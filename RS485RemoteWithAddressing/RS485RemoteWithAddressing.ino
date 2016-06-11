f84fead9c7e8f0c7c859856b4280007ed4d0c236
stevemolloy <sdmolloy@gmail.com>
2016-06-11 15:51:18 +0200

    Added a simple state

--- RS485RemoteWithAddressing/RS485RemoteWithAddressing.ino
+++ RS485RemoteWithAddressing/RS485RemoteWithAddressing.ino
@@ -1,12 +1,4 @@
-/* YourDuino SoftwareSerialExample1Remote
-   - Used with YD_SoftwareSerialExampleRS485_1 on another Arduino
-   - Remote: Receive data, loop it back...
-   - Connect this unit Pins 10, 11, Gnd
-   - To other unit Pins 11,10, Gnd  (Cross over)
-   - Pin 3 used for RS485 direction control   
-   - Pin 13 LED blinks when data is received  
-   
-   Questions: terry@yourduino.com 
+/* 
 */
 
 /*-----( Import needed libraries )-----*/
@@ -17,24 +9,27 @@
 #define SSerialTxControl 3   //RS485 Direction control
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW
+#define intPin           2
+#define statePin         5
 
 /*-----( Declare objects )-----*/
 SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX
 
 /*-----( Declare Variables )-----*/
 byte byteReceived;
-int byteSend;
-int reply;
-int frameAddr, reply1, reply2;
+int byteSend, reply, frameAddr, reply1, reply2;
 unsigned int state = 1;
 const byte addr = 0x02;
 
 void setup() {  /****** SETUP: RUNS ONCE ******/
   //pinMode(Pin13LED, OUTPUT);   
   pinMode(SSerialTxControl, OUTPUT);
+  pinMode(statePin, OUTPUT);
+  digitalWrite(statePin, LOW);
   digitalWrite(SSerialTxControl, RS485Receive);  // Init Transceiver
   // Start the software serial port, to another device
   RS485Serial.begin(9600);   // set the data rate
+  attachInterrupt(digitalPinToInterrupt(intPin), trigFunc, CHANGE);
 }//--(end setup )---
 
 void loop() {  /****** LOOP: RUNS CONSTANTLY ******/
@@ -82,5 +77,19 @@ void frameSender() {
   RS485Serial.write(reply1);
   RS485Serial.write(reply2);
   digitalWrite(SSerialTxControl, RS485Receive);  // Disable RS485 Transmit
+}
+
+void trigFunc() {
+  switch (state) {
+    case 1:
+      digitalWrite(statePin, !digitalRead(statePin));
+      break;
+    case 2:
+      digitalWrite(statePin, LOW);
+      break;
+    default:
+      digitalWrite(statePin, LOW);
+      break;
+  }
 }
 
